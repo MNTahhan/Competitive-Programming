023097622f4e318c681ecdbf52cdb8b9e2850484
MNT95 <imnt.95@gmail.com>
2017-03-12 00:08:58 +0200

    Create 3200 - Jill's Tour Paths.cpp

---
+++ Live Archive/3200 - Jill's Tour Paths.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 22;
+int n,m,vis[N],vs,s,t,d;
+vector<pair<int,int> >adj[N];
+vector<pair<int,vector<int> > >ans;
+vector<int>cur;
+void dfs(int u,int c){
+	if(vis[u]==vs)
+		return;
+	vis[u]=vs;
+	cur.push_back(u);
+	if(u==t){
+		if(c<=d)
+			ans.push_back(make_pair(c,cur));
+		vis[u]=vs-1;
+		cur.pop_back();
+		return;
+	}
+	for(int i=0;i<adj[u].size();++i)
+		dfs(adj[u][i].first,adj[u][i].second+c);
+	vis[u]=vs-1;
+	cur.pop_back();
+}
+int main() {
+	int h=1,flag=0;
+	while(scanf("%d",&n)>0 && n!=-1){
+		if(flag)
+			puts("");
+		flag=1;
+		scanf("%d",&m);
+		for(int i=0;i<=n;++i)
+			adj[i].clear();
+		for(int i=0,u,v,c;i<m;++i){
+			scanf("%d%d%d",&u,&v,&c);
+			adj[u].push_back(make_pair(v,c));
+			adj[v].push_back(make_pair(u,c));
+		}
+		scanf("%d%d%d",&s,&t,&d);
+		ans.clear();
+		cur.clear();
+		++vs;
+		dfs(s,0);
+		printf("Case %d:\n",h++);
+		if(!ans.size())
+			puts(" NO ACCEPTABLE TOURS");
+		else{
+			sort(ans.begin(),ans.end());
+			for(int i=0;i<ans.size();++i){
+				printf(" %d: ",ans[i].first);
+				for(int j=0;j<ans[i].second.size();++j)
+					printf("%d ",ans[i].second[j]);
+				puts("");
+			}
+		}
+	}
+	return 0;
+}
