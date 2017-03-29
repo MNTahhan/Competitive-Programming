fe5a7c181d9a2e1a361f7b75ad4b7e15dd5a9b0d
MNT95 <imnt.95@gmail.com>
2017-03-29 10:13:08 +0200

    Create F. black-whitw.cpp

---
+++ Codeforces/F. black-whitw.cpp
@@ -0,0 +1,52 @@
+// problem link: http://codeforces.com/gym/100947/problem/F
+
+#include <iostream>	
+#include <time.h>
+#include <vector>
+#include <stdio.h>
+#include <memory.h>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <bitset>
+#include <queue>
+#include <set>
+#include <time.h>
+#include <assert.h>
+#include <sstream>
+//#include <unordered_map>
+#include <bitset>
+#include <utility>
+#include <iomanip>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <numeric>
+#include <math.h>
+#include <cmath>
+using namespace std;
+typedef long long ll;
+
+
+
+int main() {
+#if !ONLINE_JUDGE
+	freopen("a.txt", "r", stdin);
+	//freopen("a.txt", "w", stdout);
+#endif
+	
+	int t;
+	scanf("%d",&t);
+	while(t--){
+		int n,w,b;
+		scanf("%d%d%d",&n,&w,&b);
+		if((abs(w-b)-1)&1)
+			puts("Steven");
+		else
+			puts("Mikael");
+	}
+
+
+	return 0;
+}
