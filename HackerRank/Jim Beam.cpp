9dd9472f3555b0ebf73fe7e7cdf5381d58b54cad
MNT95 <imnt.95@gmail.com>
2017-06-16 20:59:07 +0300

    Create Jim Beam.cpp

---
+++ HackerRank/Jim Beam.cpp
@@ -0,0 +1,101 @@
+#include <iostream>	
+#include <time.h>
+#include <vector>
+#include <stdio.h>
+#include <memory.h>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <bitset>
+#include <queue>
+#include <set>
+#include <time.h>
+#include <assert.h>
+#include <sstream>
+#include <unordered_map>
+#include <bitset>
+#include <utility>
+#include <iomanip>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <numeric>
+#include <math.h>
+#include <cmath>
+#include <complex>
+#if !ONLINE_JUDGE
+#include "inc.h"
+#endif
+using namespace std;
+typedef long long ll;
+
+const double PI = acos(-1.0);
+
+const double EPS = 1e-9;
+
+typedef complex<double> point;
+
+#define X real()
+#define Y imag()
+#define angle(a)                (atan2((a).imag(), (a).real()))
+#define vec(a,b)                ((b)-(a))
+#define same(p1,p2)             (dp(vec(p1,p2),vec(p1,p2)) < EPS)
+#define dp(a,b)                 ( (conj(a)*(b)).real() )	// a*b cos(T), if zero -> prep
+#define cp(a,b)                 ( (conj(a)*(b)).imag() )	// a*b sin(T), if zero -> parllel
+#define length(a)               (hypot((a).imag(), (a).real()))
+#define normalize(a)            (a)/length(a)
+//#define polar(r,ang)            ((r)*exp(point(0,ang)))  ==> Already added in c++11
+#define rotateO(p,ang)          ((p)*exp(point(0,ang)))
+#define rotateA(p,ang,about)  (rotateO(vec(about,p),ang)+about)
+#define reflectO(v,m)  (conj((v)/(m))*(m))
+int ccw(point a, point b, point c) {
+	point v1(b - a), v2(c - a);
+	double t = cp(v1, v2);
+
+	if (t > +EPS)
+		return +1;
+	if (t < -EPS)
+		return -1;
+	if (v1.X * v2.X < -EPS || v1.Y * v2.Y < -EPS)
+		return -1;
+	if (norm(v1) < norm(v2) - EPS)
+		return +1;
+	return 0;
+}
+bool intersect(point p1, point p2, point p3, point p4) {
+	// special case handling if a segment is just a point
+	bool x = (p1 == p2), y = (p3 == p4);
+	if (x && y)  return p1 == p3;
+	if (x)   return ccw(p3, p4, p1) == 0;
+	if (y)   return ccw(p1, p2, p3) == 0;
+
+	return  ccw(p1, p2, p3) * ccw(p1, p2, p4) <= 0 &&
+		ccw(p3, p4, p1) * ccw(p3, p4, p2) <= 0;
+}
+
+int main() {
+#if !ONLINE_JUDGE
+	freopen("a.txt", "r", stdin);
+	//freopen("b.txt", "w", stdout);
+	decTime;
+#endif
+
+	int t;
+	scanf("%d", &t);
+	while (t--) {
+		vector<int>v(6);
+		for (int i = 0; i < 6; ++i)
+			scanf("%d", &v[i]);
+		point a(v[0], v[1]);
+		point b(v[2], v[3]);
+		point c(v[4], v[5]);
+		puts(!intersect(a, b, c, point(0, 0)) ? "YES" : "NO");
+	}
+
+#if !ONLINE_JUDGE
+	printTime;
+#endif
+	return 0;
+}
+
