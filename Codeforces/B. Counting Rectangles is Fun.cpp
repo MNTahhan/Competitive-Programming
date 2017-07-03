df2f09efca3209ee740d18dfae98d8519ed332f4
MNT95 <imnt.95@gmail.com>
2017-07-03 09:59:03 +0300

    Create B. Counting Rectangles is Fun.cpp

---
+++ Codeforces/B. Counting Rectangles is Fun.cpp
@@ -0,0 +1,40 @@
+/*
+  The idea: consider the rectangle from every point to the end of the big rectangle, calc the cumulative sum of rectangles in this
+            rectangle, then in every query loop on the points inside the query and add the number of rectangles from current point to 
+            the end of query rectangle.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int n,m,cum[44][44],dp[44][44][44][44];
+char s[44][44];
+int calc(int i,int j,int x,int y){
+	if(i>x || j>y) return 0;
+	return cum[x][y]-cum[x][j-1]-cum[i-1][y]+cum[i-1][j-1]==0;
+}
+int main() {
+	int q;
+	scanf("%d%d%d",&n,&m,&q);
+	for(int i=1;i<=n;++i)
+		scanf("%s",s[i]+1);
+	for(int i=1;i<=n;++i)
+	for(int j=1;j<=m;++j)
+		cum[i][j]=(s[i][j]=='1')+cum[i-1][j]+cum[i][j-1]-cum[i-1][j-1];
+	for(int i=1;i<=n;++i)
+	for(int j=1;j<=m;++j)
+	for(int x=i;x<=n;++x)
+	for(int y=j;y<=m;++y){
+		if(cum[x][y]-cum[x][j-1]-cum[i-1][y]+cum[i-1][j-1]==0)
+			++dp[i][j][x][y];
+		dp[i][j][x][y]+=dp[i][j][x-1][y]+dp[i][j][x][y-1]-dp[i][j][x-1][y-1];	
+	}
+	while(q--){
+		int a,b,c,d;
+		scanf("%d%d%d%d",&a,&b,&c,&d);
+		int ans=0;
+		for(int i=a;i<=c;++i)
+		for(int j=b;j<=d;++j)
+			ans+=dp[i][j][c][d];
+		printf("%d\n",ans);
+	}
+	return 0;
+}
