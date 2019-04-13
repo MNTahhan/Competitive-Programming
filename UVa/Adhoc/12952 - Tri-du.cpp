26e53889c27d504b8ca11eaf83de412080632217
Mohammad AlTahhan <imnt.95@gmail.com>
2019-04-13 12:37:17 +0300

    Create 12952 - Tri-du.cpp

---
+++ UVa/Adhoc/12952 - Tri-du.cpp
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>	
+#include <time.h>
+#include <vector>
+#include <stdio.h>
+#include <memory.h>
+#include <string>
+#include <string.h>
+#include <map>
+#include <algorithm>
+#include <queue>
+#include <set>
+#include <time.h>
+#include <assert.h>
+#include <sstream>
+//#include <unordered_map>
+#include <bitset>
+#include <utility>
+#include <iomanip>
+#include <climits>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <numeric>
+#include <math.h>
+#include <cmath>
+#include <complex>
+#ifdef MYDEF
+#include "inc.h"
+#endif
+using namespace std;
+typedef long long ll;
+
+
+int main() {
+#ifdef MYDEF
+	HEAD;
+	freopen("a.txt", "r", stdin);
+	//freopen("b.txt", "w", stdout);
+	decTime;
+#endif
+
+	int a, b;
+	while(scanf("%d%d", &a, &b) > 0) {
+		printf("%d\n", max(a, b));
+	}
+
+#ifdef MYDEF
+	printTime;
+#endif
+	return 0;
+}
